404015d7c5b47007dcd89ed9e6b252f88f1c908b
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2024-02-26 19:16:34 +0300

    OOP-HTML

--- OOP/jsFolders/app.js
+++ OOP/jsFolders/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+const inputValue = document.getElementById('addToDoInput')
+const addButton = document.getElementById('addButton')
+
+addEventListeners();
+
+function addEventListeners () {
+  inputValue.addEventListener('keyup', writeInput)
+  addButton.addEventListener('click', changeColor)
+}
+
+
+function changeColor () {
+  addButton.style.backgroundColor = 'red'
+}
+
+function writeInput (e) {
+  console.log(e.target.value)
+}
