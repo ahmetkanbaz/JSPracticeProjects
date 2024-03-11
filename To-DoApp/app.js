bc03473519eaa7a95e5588cbe9aafb15c150fa27
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2024-03-11 16:23:00 +0300

    To-DoTasks
    Eklenen taskler içerisinde arama işlemi gerçekleştirildi. Aynı zamanda tüm tasklerin silinebilmesi de sağlandı.

--- To-DoApp/app.js
+++ To-DoApp/app.js
@@ -1,31 +1,59 @@
-const toDoInput = document.getElementById("toDoInput");
+const inputToDo = document.getElementById("inputToDo");
 const addToDoButton = document.getElementById("addToDoButton");
+const searchToDo = document.getElementById("searchToDo");
 const listGroup = document.querySelector(".list-group");
+const clearAllTasksButton = document.getElementById("clearAllTasksButton");
+
+let tasks = [];
 
 addEventListeners();
 
 function addEventListeners() {
+  // document.addEventListener("DOMContentLoaded", getAndCheckTasks);
   // document.addEventListener('keyup', enterDeneme)
   addToDoButton.addEventListener("click", createNewTask);
+  searchToDo.addEventListener("keyup", searchTasks);
   listGroup.addEventListener("click", removeItem);
-  listGroup.addEventListener('click', updateStatusItem)
+  listGroup.addEventListener("click", updateStatusItem);
+  clearAllTasksButton.addEventListener("click", clearAllTasks);
+}
+
+function getAndCheckTasks() {
+  tasks.length == 0
+    ? listGroup.parentElement.classList.add("d-none")
+    : console.log(listGroup.parentElement.classList);
 }
 
 function createNewTask() {
-  if (toDoInput.value.trim() == "") {
+  if (inputToDo.value.trim() == "") {
     alert("Görev kısmı boş bırakılarak eklenme yapılamaz!");
   } else {
     const newLi = document.createElement("li");
     newLi.classList =
       "list-group-item d-flex justify-content-between align-items-center";
-    newLi.textContent = toDoInput.value;
+    newLi.textContent = inputToDo.value;
     const newDeleteItem = document.createElement("a");
     newDeleteItem.href = "#";
     newDeleteItem.classList = "fa-solid fa-xmark text-decoration-none";
     newLi.appendChild(newDeleteItem);
     listGroup.appendChild(newLi);
-    toDoInput.value = "";
+    inputToDo.value = "";
+    tasks.push(newLi);
   }
+}
+
+function searchTasks(e) {
+  let taskListItems = document.querySelectorAll('.list-group-item')
+  taskListItems.forEach(function (listItem) {
+    if (listItem.textContent.toUpperCase().indexOf(e.target.value.toUpperCase()) == -1) {
+      listItem.classList.add('d-none')
+      listItem.classList.remove('d-block')
+    }
+    else {
+      listItem.classList.add('d-block')
+      listItem.classList.remove('d-none')
+    }
+  })
 }
 
 function removeItem(e) {
@@ -37,10 +65,15 @@ function removeItem(e) {
   }
 }
 
-function updateStatusItem (e) {
-  if (e.target.classList.contains('list-group-item')) {
-    e.target.classList.toggle('text-decoration-line-through')
-    console.log(e.target.className)
+function updateStatusItem(e) {
+  if (e.target.classList.contains("list-group-item")) {
+    e.target.classList.toggle("text-decoration-line-through");
+  }
+}
+
+function clearAllTasks() {
+  if (confirm("Tüm görevleri silmek istediğinize emin misiniz?")) {
+    listGroup.innerHTML = "";
   }
 }
 
